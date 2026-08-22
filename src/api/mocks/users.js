c78935333b052555ad328b456a55b6202bfba5ac
Samuk515 <singhsamir515@gmail.com>
2026-08-22 20:15:25 +0545

    Add API client, mocked /users/me, and useSession hook

---
+++ src/api/mocks/users.js
@@ -0,0 +1,7 @@
+export const mockCurrentUser = {
+  id: "mock-user-id",
+  githubId: 123456,
+  username: "samir",
+  displayName: "Samir Singh",
+  avatarUrl: "https://avatars.githubusercontent.com/u/190625443?v=4",
+};
